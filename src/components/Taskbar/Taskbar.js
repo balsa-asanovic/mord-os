396ba701b689520409c532078da2a6c9c99709d6
balsa-asanovic <balsaasanovic95@gmail.com>
2022-09-23 02:27:40 +0200

    Added time and date, title to taskbar

--- src/components/Taskbar/Taskbar.js
+++ src/components/Taskbar/Taskbar.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react';
 import './taskbar.css';
 
 const Taskbar = () => {
+    const [date, setDate] = useState(() => {
+        const fullDate = new Date();
+        return fullDate.toLocaleString('default', { month: 'short' }) + " " + fullDate.getDay().toString() + " " + fullDate.getFullYear().toString();
+    });
+    const [time, setTime] = useState(() => {
+        const fullDate = new Date();
+        return fullDate.getHours().toString().padStart(2, '0') + ":" + fullDate.getMinutes().toString().padStart(2, '0') + ":" + fullDate.getSeconds().toString().padStart(2, '0');
+    });
+
+    useEffect(() => {
+        setInterval(() => {
+            const fullDate = new Date();
+            setDate(fullDate.toLocaleString('default', { month: 'short' }) + " " + fullDate.getDay().toString() + " " + fullDate.getFullYear().toString());
+            setTime(fullDate.getHours().toString().padStart(2, '0') + ":" + fullDate.getMinutes().toString().padStart(2, '0') + ":" + fullDate.getSeconds().toString().padStart(2, '0'));
+        }, 1000)
+    });
 
     return (
         <div className="taskbar-main">
-            
+            <span className="taskbar-title">Mord OS </span>
+            <span className="taskbar-date">{date}</span>
+            <span className="taskbar-time">{time}</span>
         </div>
     )
 };
