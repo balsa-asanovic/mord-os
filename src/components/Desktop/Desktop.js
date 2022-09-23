9bbe4a38ea0fc881bdb56249214de2d724a47fa1
balsa-asanovic <balsaasanovic95@gmail.com>
2022-09-23 02:40:08 +0200

    Added Icon component and drag option

--- src/components/Desktop/Desktop.js
+++ src/components/Desktop/Desktop.js
@@ -1,11 +1,15 @@
 import './desktop.css';
 import Taskbar from '../Taskbar/Taskbar';
+import Icon from '../Icon/Icon';
+import Draggable from 'react-draggable';
 
 const Desktop = () => {
 
     return (
         <div className="desktop-main">
             <Taskbar />
+            <Icon />
+
         </div>
     )
 };
---
+++ src/components/Icon/Icon.js
@@ -0,0 +1,15 @@
+import "./icon.css";
+import Draggable from 'react-draggable';
+
+const Icon = () => {
+
+    return (
+        <Draggable>
+        <div className="icon-main">
+
+        </div>
+        </Draggable>
+    )
+};
+
+export default Icon;
